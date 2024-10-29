eb9de97bb0f83e1fd94c149effaa7a84aa1d3d1e
Felipe Ribeiro <piporibeiro@gmail.com>
2024-10-29 11:02:37 -0300

    feat: add logic for filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,67 @@
-import { useEffect, useState } from "react";
-import { GlobalStyle } from "./styles/globals";
+import { useEffect, useState, lazy, Suspense } from "react";
+
 import { User } from "./interface/user";
+import { Container, HeaderContainer, GlobalStyle } from "./styles";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import { Button } from "./components/Button";
+import { Filter } from "./components/Filter";
+
+const TableUser = lazy(() => import("./components/TableUser/index"));
+const CardUser = lazy(() => import("./components/CardUser"));
 
 const initialUserState: User[] = [];
 
 function App() {
   const [users, setUsers] = useState<User[]>(initialUserState);
+  const [visualization, setVisualization] = useState("table");
+  const [filter, setFilter] = useState("");
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    const params = new URLSearchParams(location.search);
+    const filterParam = params.get("filter") || "";
+    setFilter(filterParam);
+
+    fetch(`https://jsonplaceholder.typicode.com/users?name_like=${filterParam}`)
       .then((response) => response.json())
       .then((data) => setUsers(data));
-  });
+  }, [location.search]);
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newFilter = event.target.value;
+    setFilter(newFilter);
+    const params = new URLSearchParams(location.search);
+    if (newFilter) {
+      params.set("filter", newFilter);
+    } else {
+      params.delete("filter");
+    }
+    navigate({ search: params.toString() });
+  };
+
   return (
-    <>
-      <div>
-        <GlobalStyle />
-        {users.map((user) => {
-          return (
-            <div key={user.id}>
-              <h1>{user.name}</h1>
-              <p>{user.email}</p>
-              <p>{user.phone}</p>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <div>
+      <GlobalStyle />
+      <Container>
+        <HeaderContainer>
+          <Button
+            tableVisualization={() => setVisualization("table")}
+            cardVisualization={() => setVisualization("card")}
+          />
+          <Filter filter={filter} handleFilterChange={handleFilterChange} />
+        </HeaderContainer>
+        <Suspense fallback={<div>Loading...</div>}>
+          {visualization === "table" ? (
+            <TableUser users={users} />
+          ) : (
+            <CardUser users={users} />
+          )}
+        </Suspense>
+      </Container>
+    </div>
   );
 }
 
