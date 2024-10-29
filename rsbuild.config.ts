3d960adebef3902ecf51f909dfafd0efb93a2f7c
Felipe Ribeiro <piporibeiro@gmail.com>
2024-10-29 12:37:19 -0300

    feat: add title and description

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginReact } from "@rsbuild/plugin-react";
 
+const templates = {
+  index: "./public/index.html",
+};
+
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: "Sensedia Teste",
+    meta: {
+      description: "Aplicações de teste para a Sensedia",
+    },
+  },
 });
